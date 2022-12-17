6f96b00056a513b36cd23cc05365aa135803f5d9
Kuzivashe <kuzibaye@gmail.com>
2022-12-17 02:03:13 -0800

    updated

---
+++ bytecodes/000.m
@@ -0,0 +1,9 @@
+push 0 Kuzi
+push 1 chibaba
+push 2 pavese
+  push 3 dzikamai
+  			pall 2
+push 4 .
+    push 5     Htown
+    	push	6	ma1
+pall 1
--- monty.h
+++ monty.h
@@ -4,6 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <errno.h>
 
 #define LIMIT 10
 
