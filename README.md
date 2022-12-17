b7998d6f801a9f691556448eabac333275225838
Kuzivashe <kuzibaye@gmail.com>
2022-12-17 00:50:18 -0800

    monty.h

---
+++ README.md
@@ -0,0 +1,4 @@
+MONTY
+
+Interpreter for Monty Bytecodes files.
+0x19-Stacks, Queues - LIFO, FIFO
---
+++ monty.h
@@ -0,0 +1,49 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#define LIMIT 10
+
+/**
+  * struct stack_s - doubly linked list representation
+  * of a stack or queue
+  * @n: integer
+  * @prev: points to the previous element of the stack
+  * or queue
+  * @next: points to the next element of the stack or
+  * queue
+  *
+  * Description: doubly linked list node structure
+  * for Stack, Queues, LIFO, FIFO
+  */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+  * struct instruction_s - opcode and its function
+  * @opcode: the opcode
+  * @f: function to handle the opcode
+  *
+  * Description: opcode and its function
+  * for stacks, queues, LIFO, FIFO
+  */
+typedef struct instruction_s
+{
+	char *opcode;
+	void(*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+int stack[LIMIT];
+int top = -1;
+void op_push(int element);
+void op_pall(int i);
+void op_pint(int i);
+void op_pop(int element);
+void op_swap(int a, int b);
+void op_add(int a, int b);
+void op_nop();
+
+#endif /* MONTY_H */
